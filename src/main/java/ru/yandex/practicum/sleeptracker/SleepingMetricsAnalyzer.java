b9eb7d846f91f2a9c9a2584388c237f8e6fa47a3
BatianyaVombat <alex.strange.game@yandex.ru>
2026-05-28 02:06:09 +0300

    Убрал неиспользуемый импорт

--- src/main/java/ru/yandex/practicum/sleeptracker/SleepingMetricsAnalyzer.java
+++ src/main/java/ru/yandex/practicum/sleeptracker/SleepingMetricsAnalyzer.java
@@ -4,7 +4,6 @@ import java.time.LocalTime;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 public class SleepingMetricsAnalyzer {
     private final List<SleepingSession> sessions;
